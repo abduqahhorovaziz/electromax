48c7df5320ffe87b55547e41389c7ee263231cc4
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2021-12-23 00:53:59 +0500

    project end - responsivlikka tayorlangamgan

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('.owl-one').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
@@ -15,3 +15,35 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+$('.owl-two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
